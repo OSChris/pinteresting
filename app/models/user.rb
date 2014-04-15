3d9a6fce6a2ff4281d2bd590e72eced4a5bdb8c9
Chris Yammine <chris.yammine@hotmail.com>
2014-04-15 14:12:05 -0400

    Add names to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :name, presence: true, length: {maximum: 50, minimum: 2}
+  
   has_many :pins
 end
